467c75693e04361334eba9855f0c104b61ec5d62
Reza <reza@ies-italia.it>
2020-02-08 16:23:52 +0100

    Docker & system.Exec

--- components/docker/controller.go
+++ components/docker/controller.go
@@ -103,7 +103,7 @@ func createDockerFile() {
 	dockerfile.Append("FROM iron/go:dev\n")
 	dockerfile.Append("WORKDIR /app\n")
 	dockerfile.Append("ENV SRC_DIR=" + gitPath + "/\n")
-	dockerfile.Append("ENV go get " + gitPath + "\n")
+	dockerfile.Append("RUN go get " + gitPath + "\n")
 	dockerfile.Append("ADD ./config.yml /app/\n")
 	dockerfile.Append("RUN cd $SRC_DIR; go build -o /app/" + gitName + ";\n")
 	dockerfile.Append("ENTRYPOINT [\"/app/" + gitName + "\"]\n")
