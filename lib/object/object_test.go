0cf4a90265a4ebcd27f58c3012aad67bf60d9155
reza <reza@ies-italia.it>
2020-02-18 00:39:23 +0100

    Object reflector

---
+++ lib/object/object_test.go
@@ -0,0 +1,22 @@
+package object
+
+import (
+	"fmt"
+	"testing"
+)
+
+type myStruct struct {
+	X string
+	y int
+	z int
+}
+
+func TestNew(t *testing.T) {
+	x := &myStruct{
+		"CDE", 9, 8,
+	}
+
+	Wrap(x).Set("X", "KKJKJJJ")
+
+	fmt.Println(x)
+}
